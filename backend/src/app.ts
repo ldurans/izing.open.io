830659d959f02c9177283e2ea85ce798154d7063
Durans <lumardyelson@gmail.com>
2020-12-17 11:28:54 -0300

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -37,9 +37,9 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   // Quais são os métodos que a conexão pode realizar na API
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  app.use(cors());
   next();
-});
+}, cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(Sentry.Handlers.requestHandler());
