3e1d9603d13c0d35575f8489b42c38f675391389
Durans <lumardyelson@gmail.com>
2020-12-17 11:24:57 -0300

    cors middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,7 +32,14 @@ const app = express();
 // }, cors());
 // }
 
-app.use(cors());
+app.use((req, res, next) => {
+  // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  // Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(Sentry.Handlers.requestHandler());
