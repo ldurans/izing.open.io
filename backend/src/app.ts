1b0cd15a5d22cdc12a32bac86670555631294701
Durans <lumardyelson@gmail.com>
2020-12-17 11:33:19 -0300

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,14 +32,7 @@ const app = express();
 // }, cors());
 // }
 
-app.use((req, res, next) => {
-  // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-  res.header("Access-Control-Allow-Origin", "*");
-  // Quais são os métodos que a conexão pode realizar na API
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  next();
-}, cors());
-
+app.use(cors({ origin: "*" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(Sentry.Handlers.requestHandler());
