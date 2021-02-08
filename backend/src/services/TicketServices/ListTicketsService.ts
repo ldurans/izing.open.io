700c0bbabf1734d7e7f83dd8f524c402590daa25
Durans <lumardyelson@gmail.com>
2021-02-08 20:28:36 -0300

    Retirado logging indevido da listagem de tickets.

--- backend/src/services/TicketServices/ListTicketsService.ts
+++ backend/src/services/TicketServices/ListTicketsService.ts
@@ -289,8 +289,7 @@ const ListTicketsService = async ({
     distinct: true,
     limit,
     offset,
-    order: [["updatedAt", "DESC"]],
-    logging: console.log
+    order: [["updatedAt", "DESC"]]
   });
 
   const hasMore = count > offset + tickets.length;
