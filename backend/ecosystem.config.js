e38ce52e11467ce575b5ecb88d400a012ba06690
Durans <lumardyelson@gmail.com>
2021-11-10 03:51:11 -0300

    retirqdo o restart audo do pm2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = [{
   script: 'dist/server.js',
   name: 'wchats-app',
   exec_mode: 'cluster',
-  cron_restart: '00 00 * * *',
+  // cron_restart: '00 00 * * *',
   instances: 1,
   watch: false
 }]
