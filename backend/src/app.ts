7dd6a7b5a76b84702d8650f1ea9659ea436a2c48
Lumardyelson Durans <lumardyelson@gmail.com>
2022-05-23 20:13:19 -0300

    teste cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -114,10 +114,11 @@ app.use(
         url => url.indexOf(origin) !== -1
       );
       if (allowed === -1) {
-        const msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
+        return callback(null, true);
+        // const msg =
+        //   "The CORS policy for this site does not " +
+        //   "allow access from the specified Origin.";
+        // return callback(new Error(msg), false);
       }
       return callback(null, true);
     }
