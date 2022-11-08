f7ae28ea70935a2e4c7e0b76ec6716b055b70856
Durans <lumardyelson@gmail.com>
2022-11-07 22:59:37 -0300

    fix: alterado valor defaul do ID para utilizar UUID do Postgres nas tabelas de configuração da api e mensagens da api.

--- backend/src/database/migrations/20210308153511-create-extension-uuid-ossp-postgres.ts
+++ backend/src/database/migrations/20210308153511-create-extension-uuid-ossp-postgres.ts
@@ -3,19 +3,19 @@ import { QueryInterface } from "sequelize";
 module.exports = {
   up: (queryInterface: QueryInterface) => {
     return Promise.all([
-      queryInterface.sequelize
-        .query
+      queryInterface.sequelize.query(
+        "select gen_random_uuid()"
         // 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
-        ()
+      )
     ]);
   },
 
   down: (queryInterface: QueryInterface) => {
     return Promise.all([
-      queryInterface.sequelize
-        .query
+      queryInterface.sequelize.query(
+        "select gen_random_uuid()"
         // 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
-        ()
+      )
     ]);
   }
 };
