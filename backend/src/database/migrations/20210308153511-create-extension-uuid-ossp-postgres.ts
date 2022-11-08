bf81d3258c1065609e2a462323006f009fb14f27
Durans <lumardyelson@gmail.com>
2022-11-07 22:51:37 -0300

    fix: alterado valor defaul do ID para utilizar UUID do Postgres nas tabelas de configuração da api e mensagens da api.

--- backend/src/database/migrations/20210308153511-create-extension-uuid-ossp-postgres.ts
+++ backend/src/database/migrations/20210308153511-create-extension-uuid-ossp-postgres.ts
@@ -3,17 +3,19 @@ import { QueryInterface } from "sequelize";
 module.exports = {
   up: (queryInterface: QueryInterface) => {
     return Promise.all([
-      queryInterface.sequelize.query(
-        'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
-      )
+      queryInterface.sequelize
+        .query
+        // 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+        ()
     ]);
   },
 
   down: (queryInterface: QueryInterface) => {
     return Promise.all([
-      queryInterface.sequelize.query(
-        'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
-      )
+      queryInterface.sequelize
+        .query
+        // 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+        ()
     ]);
   }
 };
---
+++ backend/src/database/migrations/20221108014456-alter_table_ApiMessages_edit_id_default_value.ts
@@ -0,0 +1,25 @@
+import { QueryInterface, DataTypes, Sequelize } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface) => {
+    return Promise.all([
+      queryInterface.changeColumn("ApiMessages", "id", {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal("gen_random_uuid()")
+      })
+    ]);
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return Promise.all([
+      queryInterface.changeColumn("ApiMessages", "id", {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal("uuid_generate_v4()")
+      })
+    ]);
+  }
+};
