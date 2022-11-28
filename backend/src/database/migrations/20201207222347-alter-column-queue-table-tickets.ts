41bd80dacd15323bdc23a0302f7b9ae4b385ffdd
Durans <lumardyelson@gmail.com>
2022-11-27 23:23:49 -0300

    refactor: ajuste nas migrations para melhorar o fluxo de execução

--- backend/src/database/migrations/20201207222347-alter-column-queue-table-tickets.ts
+++ backend/src/database/migrations/20201207222347-alter-column-queue-table-tickets.ts
@@ -1,31 +1,27 @@
 import { QueryInterface, DataTypes } from "sequelize";
 
 module.exports = {
-  up: (queryInterface: QueryInterface) => {
-    return Promise.all([
-      queryInterface.removeColumn("Tickets", "queue"),
-      queryInterface.addColumn("Tickets", "queue", {
-        type: DataTypes.INTEGER,
-        references: { model: "Queues", key: "id" },
-        onUpdate: "CASCADE",
-        onDelete: "restrict",
-        defaultValue: null,
-        allowNull: true
-      })
-    ]);
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.removeColumn("Tickets", "queue");
+    await queryInterface.addColumn("Tickets", "queue", {
+      type: DataTypes.INTEGER,
+      references: { model: "Queues", key: "id" },
+      onUpdate: "CASCADE",
+      onDelete: "restrict",
+      defaultValue: null,
+      allowNull: true
+    });
   },
 
-  down: (queryInterface: QueryInterface) => {
-    return Promise.all([
-      queryInterface.removeColumn("Tickets", "queue"),
-      queryInterface.addColumn("Tickets", "queue", {
-        type: DataTypes.INTEGER,
-        // references: { model: "ServiceQueue", key: "id" },
-        // onUpdate: "CASCADE",
-        // onDelete: "CASCADE",
-        defaultValue: null,
-        allowNull: true
-      })
-    ]);
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.removeColumn("Tickets", "queue");
+    await queryInterface.addColumn("Tickets", "queue", {
+      type: DataTypes.INTEGER,
+      // references: { model: "ServiceQueue", key: "id" },
+      // onUpdate: "CASCADE",
+      // onDelete: "CASCADE",
+      defaultValue: null,
+      allowNull: true
+    });
   }
 };
