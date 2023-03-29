cab1744a5085d15bdcf1771e527e893912e53172
Lumardyelson Durans <lumardyelson@gmail.com>
2023-03-29 11:26:14 -0300

    delete admin

--- admin-frontend/src/boot/apex.js
+++
@@ -1,8 +0,0 @@
-import VueApexCharts from 'vue-apexcharts'
-
-export default ({
-  Vue
-}) => {
-  Vue.use(VueApexCharts)
-  Vue.component('apexchart', VueApexCharts)
-}
--- admin-frontend/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-
-const getters = {
-  whatsapps: state => state.whatsapp.whatsApps,
-  isSuporte: state => state.user.isSuporte,
-  isAdmin: state => state.user.isAdmin
-}
-export default getters
