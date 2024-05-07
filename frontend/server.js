795101d78306f579438ae193aec9685441bbaa2c
cleitonme <cleitonme@gmail.com>
2024-05-07 09:48:38 -0300

    facilitar instalacao com server.js frontend

---
+++ frontend/server.js
@@ -0,0 +1,11 @@
+// simple express server to run frontend production build;
+const express = require('express')
+const path = require('path')
+const app = express()
+app.use(express.static(path.join(__dirname, 'dist/pwa')))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/pwa', 'index.html'))
+})
+app.listen(4444)
+
+
