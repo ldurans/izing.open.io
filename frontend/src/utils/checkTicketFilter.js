4084b91a0c8ef6cc4e616a720f8998117bd6e2f0
cleitonme <cleitonme@gmail.com>
2024-05-11 21:10:48 -0300

    Update checkTicketFilter.js

--- frontend/src/utils/checkTicketFilter.js
+++ frontend/src/utils/checkTicketFilter.js
@@ -3,7 +3,7 @@ const checkTicketFilter = (ticket) => {
   const filtroPadrao = {
     searchParam: '',
     pageNumber: 1,
-    status: ['open', 'pending'],
+    status: ['open', 'pending', 'closed'],
     showAll: false,
     count: null,
     queuesIds: [],
